9603967d2d505f63c0ea65868eb404f532093bea
Ethan <ethan.t.schmidt@gmail.com>
2018-07-30 17:54:42 -0400

    more

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+# https://www.digitalocean.com/community/tutorials/how-to-make-a-simple-calculator-program-in-python-3
+
 number_1 = input("Enter your first number:  ")
 number_2 = input("Enter your second number:  ")
 
