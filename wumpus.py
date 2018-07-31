fd58e621eb9cb203b1caa45ce1149f72553657c0
Ethan <ethan.t.schmidt@gmail.com>
2018-07-31 12:55:06 -0400

    asdf

--- wumpus.py
+++ wumpus.py
@@ -1,44 +1,58 @@
 from random import randint
+from random import sample
 
 def start_hunt():
 	
 	#initialize variables
 	current_room = randint(1,21)
 
-	print("""
+	print('''
 		You awake in the dread Wumpus lair
 		The stench of death in rank in the air
 		Hunt the Wumpus; he is hunting you;
 		Kill the Wumpus, ere he kills you!
-		""")
+		''')
+
+# initialize room options
+def set_room_options():
+	room_options = sample(range(1,21), 3)
+	global option_1
+	option_1 = room_options[0]
+	global option_2	
+	option_2 = room_options[1]
+	global option_3
+	option_3 = room_options[2]
 
 #choose the next room to move
 def choose_room():
 
-	option_1 = randint(1,21)
-	option_2 = randint(1,21)
-	option_3 = randint(1,21)
-
-	room_choice = int(input("""
+	global room_choice
+	room_choice = int(input('''
 		Which room to go next? {room_one}, {room_two}, {room_three}
-		""".format(room_one=str(option_1), 
+		'''.format(room_one=str(option_1), 
 							 room_two=str(option_2), 
 							 room_three=str(option_3)
 							)
 		))
 
-	if room_choice in [option_1, option_2, option_3]:
-		current_room = room_choice 
+	if room_choice in [option_1, option_2, option_3]: 
+		return room_choice
 	else:
-		print("""
-			invalid room choice
-			""")
-
+		print('''
+			Invalid room choice \
+			''')
+		
 #move to new room
 def move_room():
-	current_room = room_choice
+	current_choice = room_choice
+
+	print('''
+		This room smells worse than the last...
+		''')
 
 #run hunt
 start_hunt()
+
+set_room_options()
 
 choose_room()
